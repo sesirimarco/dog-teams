3aa7212b2365e6c760ad3b15dfe1bb2c3cd2aaea
sesirimarco <sesirimarco@gmail.com>
2021-02-25 17:44:21 -0300

    chore: add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 function App() {
   return (
     <div className="App">
-      <h2>Challenge</h2>
+      <Container>
+        <Row className="justify-content-center">
+            <h2>Challenge</h2>
+        </Row>
+        
+      </Container>
     </div>
   );
 }
