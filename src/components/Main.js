dcb20983bc193e22b436fe2feb16b58707a66f57
sesirimarco <sesirimarco@gmail.com>
2021-02-27 15:29:30 -0300

    feat: main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Search from './Search';
 
 const Main = () => {
   return (
@@ -16,16 +14,9 @@ const Main = () => {
     </Row>
     <Row className="py-5 text-center">
       <Col className="col-sm-12">
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="basic-addon2"
-          />
-          <InputGroup.Append>
-            <Button variant="outline-secondary">Button</Button>
-          </InputGroup.Append>
-        </InputGroup>
+        <Search 
+          handlerSearch={(result) => { console.log(result)}}
+        />
       </Col>
     </Row>
     <hr/>
