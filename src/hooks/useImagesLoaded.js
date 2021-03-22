e7aa594ebf9bf1de4ae4f0cec1e2f2e441d31b58
sesirimarco <sesirimarco@gmail.com>
2021-03-22 14:01:14 -0300

    fix: warnings

--- src/hooks/useImagesLoaded.js
+++ src/hooks/useImagesLoaded.js
@@ -1,19 +1,20 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 export const useImagesLoaded = () => {
   const [loadedImages, setLoadedImages] = useState(0);
   const [totalImages, setTotalImagesToLoad] = useState(0);
   const [allImagesDonde, setAllImagesDonde] = useState(false);
   
-  const setImageLoaded = useCallback(() => {
+  const setImageLoaded = () => {
     setLoadedImages(loadedImages + 1);
-  });
-  const setTotalImages = useCallback((amount) => {
+  };
+  const setTotalImages = (amount) => {
     setTotalImagesToLoad(amount);
-  });
+  };
   useEffect(() => {
     if (totalImages > 0 && totalImages === loadedImages) {
       setAllImagesDonde(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps,
   }, [loadedImages]);
   const calcPorc = (total, partial) => {
     if(total > 0 && partial > 0){
